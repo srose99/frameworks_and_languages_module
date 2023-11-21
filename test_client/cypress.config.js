e8d5e0d498615b994b5ec3ba9f9f42054c606a6f
Sam Rose <100201980+srose99@users.noreply.github.com>
2023-11-21 10:16:27 +0000

    Changed base URL for client tests as it still cannot verify server is running

--- test_client/cypress.config.js
+++ test_client/cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress")
 module.exports = defineConfig({
     e2e: {
+        baseUrl: "https://bug-free-acorn-r56q9wg9rwg25r46-8001.app.github.dev/?api=https://bug-free-acorn-r56q9wg9rwg25r46-8000.app.github.dev",
         supportFile: false,
         specPattern: 'cypress/**/*.cy.js',
         video: true,
