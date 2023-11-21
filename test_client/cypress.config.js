50ad8bf3b22fb4cb00e56b362029f5450e8cb1da
Sam Rose <100201980+srose99@users.noreply.github.com>
2023-11-21 11:00:56 +0000

    Reverted cypress config changes for testing

--- test_client/cypress.config.js
+++ test_client/cypress.config.js
@@ -1,16 +1,14 @@
 const { defineConfig } = require("cypress")
 module.exports = defineConfig({
     e2e: {
-        baseUrl: "https://bug-free-acorn-r56q9wg9rwg25r46-8001.app.github.dev/?api=https://bug-free-acorn-r56q9wg9rwg25r46-8000.app.github.dev",
         supportFile: false,
-        specPattern: 'cypress/**/*.cy.js',
+        specPattern: "cypress/**/*.cy.js",
         video: true,
-        screenshotsFolder: 'reports/screenshots',
-        videosFolder: 'reports/videos',
+        screenshotsFolder: "reports/screenshots",
+        videosFolder: "reports/videos",
         chromeWebSecurity: false,
-        reporter: 'junit',
+        reporter: "junit",
         reporterOptions: {
-            mochaFile: "reports/junit-[hash].xml"
-        },
+          mochaFile: "reports/junit-[hash].xml",
     },
 })
