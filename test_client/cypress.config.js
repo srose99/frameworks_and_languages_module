779d6b1b808b19277f7b2ec82fa6a07a9a5dc9fa
Sam Rose <100201980+srose99@users.noreply.github.com>
2023-11-27 20:43:09 +0000

    Reformat changes to attempt to revert client test errors

--- test_client/cypress.config.js
+++ test_client/cypress.config.js
@@ -1,15 +1,16 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 module.exports = defineConfig({
-  e2e: {
-    supportFile: false,
-    specPattern: "cypress/**/*.cy.js",
-    video: true,
-    screenshotsFolder: "reports/screenshots",
-    videosFolder: "reports/videos",
-    chromeWebSecurity: false,
-    reporter: "junit",
-    reporterOptions: {
-      mochaFile: "reports/junit-[hash].xml",
-    },
-  },
-});
+    e2e: {
+        supportFile: false,
+        specPattern: 'cypress/**/*.cy.js',
+        screenshotOnRunFailure: true,
+        screenshotsFolder: 'reports/screenshots',
+        video: true,
+        videosFolder: 'reports/videos',
+        chromeWebSecurity: false,
+        reporter: 'junit',
+        reporterOptions: {
+            mochaFile: 'reports/junit-[hash].xml'
+        },
+    }
+})
--- test_client/cypress/freecycle.cy.js
+++ test_client/cypress/freecycle.cy.js
@@ -1,94 +1,89 @@
-describe("FreeCycle", () => {
-	const is_a_number = (value) => {
-	  expect(Number.isNaN(+value), "input should be a number").to.eq(false);
-	};
-  
+describe('FreeCycle', () => {
+
+	const is_a_number = value => {
+		expect(Number.isNaN(+value), 'input should be a number').to.eq(false)
+	}
+
+
 	// FreeCycle Commands ------------------------------------------------------
-	Cypress.Commands.add("item_field_entry", (kwargs) => {
-	  for (let [k, v] of Object.entries(kwargs)) {
-		cy.get(`[name="${k}"]`).filter(":visible").clear().type(v);
-	  }
-	});
-  
-	Cypress.Commands.add("create_item", (kwargs) => {
-	  const uuid = Cypress._.random(0, 1e6);
-	  kwargs = {
-		...{
-		  user_id: "test_user", // HACK - Simplify Client
-		  lat: "1",
-		  lon: "1",
-		  description: "item from cypress test",
-		  image: "http://placekitten.com/100/100",
-		  keywords: "item1, item2, item3",
-		},
-		...kwargs,
-	  };
-	  kwargs.description += uuid;
-	  cy.item_field_entry(kwargs);
-	  cy.get(`[data-action="create_item"]`).filter(":visible").click();
-	  return cy
-		.contains(uuid)
-		.should("be.visible")
-		.parents("li")
-		.find('[data-field="id"]')
-		.invoke("text");
-	});
-  
-	Cypress.Commands.add("delete_item", (item_id) => {
-	  cy.contains(`[data-field="id"]`, item_id).should("exist");
-	  cy.contains(`[data-field="id"]`, item_id)
-		.parents("li")
-		.find(`[data-action="delete"]`)
-		.click();
-	  cy.contains(`[data-field="id"]`, item_id).should("not.exist");
-	});
-  
+	Cypress.Commands.add('item_field_entry', (kwargs) => {
+		for (let [k,v] of Object.entries(kwargs)) {
+			cy.get(`[name="${k}"]`).filter(':visible').clear().type(v)
+		}
+	})
+
+	Cypress.Commands.add('create_item', (kwargs) => {
+		const uuid = Cypress._.random(0, 1e6);
+		kwargs = {
+			...{
+				user_id: 'test_user',   // HACK - Simplify Client
+				lat: '1',
+				lon: '1',
+				description: 'item from cypress test',
+				image: 'http://placekitten.com/100/100',
+				keywords: 'item1, item2, item3',
+			},
+			...kwargs,
+		}
+		kwargs.description += uuid
+		cy.item_field_entry(kwargs)
+		cy.get(`[data-action="create_item"]`).filter(':visible').click()
+		return cy.contains(uuid).should('be.visible').parents('li').find('[data-field="id"]').invoke('text')
+	})
+
+	Cypress.Commands.add('delete_item', (item_id) => {
+		cy.contains(`[data-field="id"]`, item_id).should('exist');
+		cy.contains(`[data-field="id"]`, item_id).parents("li").find(`[data-action="delete"]`).click();
+		cy.contains(`[data-field="id"]`, item_id).should('not.exist');
+	})
+
 	// Each --------------------------------------------------------------------
-  
+
 	beforeEach(() => {
-	  cy.visit(""); // Navigate to Env variable `CYPRESS_BASE_URL`
-	});
-  
+		cy.visit('');  // Navigate to Env variable `CYPRESS_BASE_URL`
+	})
+
 	// Tests -------------------------------------------------------------------
-  
-	it("root page loads without error", () => {});
-  
-	it("root page contains text freecycle", () => {
-	  cy.contains("freecycle", { matchCase: false });
-	});
-  
-	it("contains input fields and submit button", () => {
-	  cy.item_field_entry({
-		user_id: "test",
-		lat: "test",
-		lon: "test",
-		description: "test",
-		image: "test",
-		keywords: "test",
-	  });
-	  cy.get(`[data-action="create_item"]`).filter(":visible");
-	});
-  
-	it("post results in item visible", () => {
-	  cy.contains("this should be visible").should("not.exist");
-	  cy.item_field_entry({
-		user_id: "test",
-		lat: "1",
-		lon: "1",
-		description: "this should be visible",
-		image: "http://placekitten.com/100/101",
-		keywords: "test",
-	  });
-	  cy.get(`[data-action="create_item"]`).filter(":visible").click();
-	  cy.get(`ul`)
-		.filter(":visible")
-		.contains("this should be visible")
-		.should("exist");
-	});
-  
-	it("Create and Delete Item", () => {
-	  cy.create_item().then((item_id) => {
-		cy.delete_item(item_id);
-	  });
-	});
-  });
+
+
+	it('root page loads without error', () => {
+	})
+
+	it('root page contains text freecycle', () => {
+		cy.contains('freecycle', { matchCase: false })
+	})
+
+	it('contains input fields and submit button', () => {
+		cy.item_field_entry({
+			user_id: 'test',
+			lat: 'test',
+			lon: 'test',
+			description: 'test',
+			image: 'test',
+			keywords: 'test',
+		})
+		cy.get(`[data-action="create_item"]`).filter(':visible')
+	})
+
+	it('post results in item visible', () => {
+		cy.contains('this should be visible').should('not.exist')
+		cy.item_field_entry({
+			user_id: 'test',
+			lat: '1',
+			lon: '1',
+			description: 'this should be visible',
+			image: 'http://placekitten.com/100/101',
+			keywords: 'test',
+		})
+		cy.get(`[data-action="create_item"]`).filter(':visible').click()
+		cy.get(`ul`).filter(':visible').contains('this should be visible').should('exist')
+	})
+
+	it('Create and Delete Item', () => {
+		cy.create_item()
+			.then(item_id => {
+				cy.delete_item(item_id);
+			})
+	})
+
+})
